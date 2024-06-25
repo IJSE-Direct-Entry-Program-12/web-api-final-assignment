4cbc274c68cbe2e6a60175bea851d6f18fd7db8c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-06-25 18:58:26 +0530

    Stage 1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 /* Enable Tooltip Texts */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$("#btn-new-employee").on('click', ()=>{
+    $("form").trigger("reset");
+    $("#txt-id").val(generateNewId());
+    $("#txt-name").trigger("focus");
+    $("#btn-save").prop("disabled", false);
+});
+
+function generateNewId(){
+   return 'E-' + ((+$("#tbl-employee > tbody > tr:last-child > td:first-child")
+        .text().replace('E-', '') + 1) + "").padStart(3, 0);
+}
+
+$("form").on('submit', (e)=>{
+    e.preventDefault();
+
+
+}).on('reset', ()=>{
+   $("#btn-save").prop("disabled", true);
+});
